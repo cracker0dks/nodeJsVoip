7136b81602b25d1bda1ffbd9f730f64e6de06e6b
Raphael <raphael@cloud13.de>
2016-07-16 17:22:52 +0200

    change from ws to socketIo and Express

--- server.js
+++ server.js
@@ -1,132 +1,60 @@
-var thePort = 9002;
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: thePort});
-var clientIds = [];
-for(var i=0;i<255;i++) {
-	clientIds.push(i);
-}
+/* CONFIG */
+var SSLPORT = 443; //Default 443
+var HTTPPORT = 80; //Default 80 (Only used to redirect to SSL port)
+var privateKeyPath = "./certs/key.pem"; //Default "./certs/key.pem"
+var certificatePath = "./certs/cert.pem"; //Default "./certs/cert.pem"
 
-var clients = [];
-var channelList = {
-	"id" : 0,
-	"zindex" : 0,
-	"name" : "root",
-	"subchannels" : []
-}
-wss.on('connection', function(ws) {
-	var client = {
-				"i" :  {
-					"clientId" : clientIds[0],
-					"ci" : 0, 	//channel id
-					"pl" : 0,	//Permission level
-					"pp" : null, //Profil Pic
-					"nn" : "user"+clients.length //Nickname
-				},
-				"ws" : ws
-			};
-	clients.push(client);
-	console.log(client["i"]["clientId"], "connected! ClientCount:"+clients.length);
-	clientIds.splice(0,1);
-	sendClientInfoList();
-		
-    ws.on('message', function(message) {
-		//console.log(message);
-		if(typeof(message)==="object") {
-			var data = addClientInformation(message, client["i"]["clientId"]);			
-			//console.log("-e", data);
-			wss.sendToChannel(client["i"]["clientId"],client["i"]["ci"], data);
-		} else {
-			//String incoming
-			console.log("inc",client["i"]["clientId"], message);
-			var msgs = message.split("###");
-			switch(msgs[0]) {
-				case "client": 
-					client["i"][msgs[1]] = msgs[2];
-					sendClientInfoList();
-					break;
-				case "chat":
-					wss.broadcast(client["i"]["clientId"], "chat###"+client["i"].nn+"###"+msgs[1]);
-					break;
-			}
-		}
-    });
-	
-	ws.on("close", function() {
-		client.ws = null;
-		console.log("kill", client["i"]["clientId"]);
-		for(var i=0; i<clients.length;i++) {
-			if(clients[i]["i"]["clientId"] == client["i"]["clientId"]) {
-				clients.splice(i,1);
-				clientIds.push(client["i"]["clientId"]);
-				break;
-			}
-		}
-		console.log('Subscriber left: ' + clients.length + " total.\n");
-		sendClientInfoList();
-	});
+//-------------------------
+var express = require('express');
+var https = require('https');
+var app = express();
 
-	ws.on('error', function(reason, code) {
-	    console.log('socket error: reason ' + reason + ', code ' + code);
-	});
+app.use(function(req, res, next) {
+    "use strict";
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
+    res.header('Access-Control-Allow-Headers', 'origin, content-type', 'X-Requested-With');
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
 });
 
-wss.broadcast = function(clientId, data) { //Send all but not the client the message was from
-    for(var i =0;i<clients.length;i++) {
-		if(clientId != clients[i].i.clientId) {
-			try {
-				clients[i]["ws"].send(data);
-			} catch(e) {
-				console.log("client already closed!");
-			}
+app.use(express.static(__dirname + '/webcontent'));
+
+var privateKey = fs.readFileSync( privateKeyPath );
+var certificate = fs.readFileSync( certificatePath );
+
+var server = https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(PORT);
+
+var io  = require('socket.io').listen(server, { log: false });
+
+// Redirect from http to https
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + ":"+ SSLPORT + "" + req.url });
+    res.end();
+}).listen(HTTPPORT);
+
+console.log("Webserver & Socketserver running on port: "+SSLPORT+ " and "+ HTTPPORT);
+
+//Handel connections
+io.sockets.on('connection', function (socket) {
+	console.log("New user connected:", socket.id);
+
+	socket.on('disconnect', function () {
+		console.log("User disconnected:", socket.id);
+	});
+
+	socket.on('d', function (data) {
+		var newData = {
+			"sid" : socket.id,
+			"a" : data //Audio data
 		}
-	}
-};
-
-wss.sendAll = function(data) {
-	for(var i =0;i<clients.length;i++) {
-		try {
-			clients[i]["ws"].send(data);
-		} catch(e) {
-			console.log("client already closed!");
-		}
-	}
-}
-
-wss.sendToChannel = function(clientId, channelId, data) {
-    for(var i =0;i<clients.length;i++) {
-		if(clientId != clients[i]["i"]["clientId"] && clients[i]["i"]["ci"]==channelId){
-			try {
-				clients[i]["ws"].send(data);
-			} catch(e) {
-				console.log("client already closed!");
-			}
-		}
-	}
-};
-
-
-console.log("Running on Port:"+thePort);
-
-/*---------------------------------------------------
-		--- Add client information to audio datas ---
----------------------------------------------------*/
-
-function sendClientInfoList() {
-	var sendObject = [];
-	for(var i=0;i<clients.length;i++) {
-		sendObject.push(clients[i].i);
-	}
-	wss.sendAll("clientlist###"+JSON.stringify(sendObject));
-}
-
-function addClientInformation(data, clientId){
-	var buff = new ArrayBuffer(data.length+1);
-	var newView = new Uint8Array(buff);
-	var oldView = new Uint8Array(data);
-	var i=0;
-	for(i = 0;i<oldView.length;i++){
-	    newView[i] = oldView[i];
-	}
-	newView[i++] = clientId;
-	return new Buffer(newView);
-}
+		socket.broadcast.emit('d', newData);
+	});
+});
